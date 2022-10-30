1be218936d48c8346e85efb1c8d7afda6e2c2890
DESKTOP-NFKEKDB\guylevy <guylevy2307@gmail.com>
2022-10-30 11:10:47 +0200

    adding list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import React,{useState} from "react";
 import './App.css';
 import AddUsers from "./components/Users/AddUsers";
+import UsersList from "./components/Users/UsersList";
+
 function App() {
+   const [userList,setUserList]= useState([]);
+   const addUserHandler=(uName,uAge)=>{
+       setUserList((prevList)=>
+       {
+           return [...prevList,{name:uName,age:uAge} ]
+       });
+   }
   return (
     <div className="App">
-        <AddUsers/>
+        <AddUsers onAddUser={addUserHandler}/>
+        <UsersList users={userList}/>
     </div>
   );
 }
